12552576db624b203443b222121e2bf0844b3b1a
alaxandre uvsq <229212122+skreeklavic@users.noreply.github.com>
2026-04-06 21:34:03 +0200

    Update GESTION.py
    début des fonctions enregistrer un coup et annulation d'un coup

--- GESTION.py
+++ GESTION.py
@@ -1,1 +1,20 @@
 
+historique =[]
+
+def enregistré_un_coup(ligne,colone):  ###pile
+    coup = (ligne,colone)
+    historique.append(coup)
+
+
+def annulation_coup(plateau):  #trouver le bon nom
+    plateau
+    if len(historique)==0:
+        print("IMPOSSIBLE AUCUN COUPS N'A ETE JOUé")
+        return
+    if len(historique) >0:
+        ligne_avant, colone_avant = historique.pop()
+
+
+    plateau[ligne_avant][colone_avant]=0
+
+    print("coup annulé en ligne = ",ligne_avant,"et colone = ",colone_avant)
