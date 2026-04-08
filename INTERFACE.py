cc7741f41accf4ee282543ec0a4eee84c9745c3d
daryss <126412930+daryss@users.noreply.github.com>
2026-04-08 11:39:38 +0200

    Update INTERFACE.py
    pas fini

--- INTERFACE.py
+++ INTERFACE.py
@@ -1,1 +1,44 @@
+LIGNES = 6
+COLONNES = 7
+TAILLE_CASE = 80
+COULEUR_VIDE = "white"
+COULEUR_JOUEUR1 = "red"
+COULEUR_JOUEUR2 = "yellow"
 
+
+grille = []
+
+for i in range(LIGNES):
+    ligne = [0] * COLONNES
+    grille.append(ligne)
+
+
+def dessiner_jeton(ligne,colonne):
+    #Dessine un jeton
+    centre_x = colonne * TAILLE_CASE + TAILLE_CASE // 2
+    centre_y = ligne * TAILLE_CASE + TAILLE_CASE // 2
+    if grille[ligne][colonne] == 1:
+        couleurs = COULEUR_JOUEUR1
+    else:
+        couleurs = COULEUR_JOUEUR2
+    
+    canvas.create_oval(centre_x - 50,centre_y + 50, centre_x + 50, centre_y - 50, fill = couleurs)
+def dessiner_grille():
+    #Dessine la grille de jeu 
+
+    for ligne in range(LIGNES):
+        for colonne in range(COLONNES):
+            x1 = colonne * TAILLE_CASE
+            y1 = ligne * TAILLE_CASE
+            x2 = x1 + TAILLE_CASE
+            y2 = y1 + TAILLE_CASE
+            canvas.create_rectangle(x1, y1, x2, y2, fill="blue")
+
+
+
+racine = tk.Tk()
+racine.title("Puissance 4")
+canvas = tk.Canvas(racine,background=COULEUR_VIDE, width = LIGNES * 100, height=COLONNES * 100 )
+racine.minsize(600, 600)
+dessiner_grille.grid(row = 0, column = 0)
+racine.mainloop()
