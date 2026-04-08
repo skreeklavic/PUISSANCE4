f4b4363740bb5b1c4efdb4a5caabee86f3cf433a
daryss <126412930+daryss@users.noreply.github.com>
2026-04-08 19:38:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import INTERFACE
 import LOGIQUE
 import GESTION
+
+
+
+
+
+
+
+racine = tk.Tk()
+racine.title("Puissance 4")
+canvas = tk.Canvas(racine,background=COULEUR_VIDE, width = LIGNES * 100, height=COLONNES * 100 )
+racine.mainloop()
