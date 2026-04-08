fffb30cdf126a1e829f41a1eaf7d5944f1953552
alaxandre uvsq <229212122+skreeklavic@users.noreply.github.com>
2026-04-08 15:11:21 +0200

    Update GESTION.py
    selection nombre de manches gagnante

--- GESTION.py
+++ GESTION.py
@@ -18,3 +18,13 @@ def annulation_coup(plateau):  #trouver le bon nom
     plateau[ligne_avant][colone_avant]=0
 
     print("coup annulé en ligne = ",ligne_avant,"et colone = ",colone_avant)
+
+
+parametres = { "manches_gagnantes": 1, "lignes": 6, "colonnes": 7 }
+
+def nombreDeMANcheGAgnante(nombreShouaite): ###choisi et verifier le nombre de manche gagnante
+    if type(nombreShouaite) == int and nombreShouaite > 0:
+        parametres["manches_gagnantes"]= nombreShouaite
+        print("dorénavant il faut ",nombreShouaite," manches pour gagné")
+    else:
+        print("Erreur : Le nombre de manches doit être un entier positif")
