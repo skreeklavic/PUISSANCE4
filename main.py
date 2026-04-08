455e1dad5783e09a67ccfa88f1701786403fcfb6
daryss <126412930+daryss@users.noreply.github.com>
2026-04-08 20:21:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import GESTION
 
 racine = tk.Tk()
 racine.title("Puissance 4")
-canvas = tk.Canvas(racine,background=COULEUR_VIDE, width = LIGNES * 100, height=COLONNES * 100 )
+canvas = tk.Canvas(racine,background=COULEUR_VIDE, width = LIGNES * TAILLE_CASE, height=COLONNES * TAILLE_CASE )
 canvas.pack()
 dessine_grille()
 racine.mainloop()
