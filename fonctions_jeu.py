bf95245c0f23e1469897b4f9ee8635ea57a5e769
saraelkh <sara.elkharrate@gmail.com>
2026-05-06 12:08:33 +0200

    Update fonctions_jeu.py

--- fonctions_jeu.py
+++ fonctions_jeu.py
@@ -1,23 +1,42 @@
-### Sarah
+### Sara
 
+##choisir aléatoirement qui commence
 import random
 
 def choisir_premier_joueur():
     return random.randint(1, 2)
 
 
+##détection de la victoire
+def verifier_victoire(grille, joueur):
+    for r in range(6):
+        for c in range(4):
+            if grille[r][c] == joueur and grille[r][c+1] == joueur and \
+               grille[r][c+2] == joueur and grille[r][c+3] == joueur:
+                return True
 
-def verifier_victoire():
+    for c in range(7):
+        for r in range(3):
+            if grille[r][c] == joueur and grille[r+1][c] == joueur and \
+               grille[r+2][c] == joueur and grille[r+3][c] == joueur:
+                return True
 
-      
+    for c in range(4):
+        for r in range(3):
+            if grille[r][c] == joueur and grille[r+1][c+1] == joueur and \
+               grille[r+2][c+2] == joueur and grille[r+3][c+3] == joueur:
+                return True
+
+    for c in range(4):
+        for r in range(3, 6):
+            if grille[r][c] == joueur and grille[r-1][c+1] == joueur and \
+               grille[r-2][c+2] == joueur and grille[r-3][c+3] == joueur:
+                return True
+
+    return False
 
 
-
-
-
-
-
-
+##détection du match nul
 def verifier_match_nul(grille):
     for c in range(7):
         if grille[0][c] == 0: 
@@ -25,7 +44,7 @@ def verifier_match_nul(grille):
     return True
       
       
-      
+##affichage du message de fin      
 def afficher_resultat(vainqueur):
     if vainqueur == 0:
         print("Match nul ! La grille est pleine.") 
@@ -34,4 +53,4 @@ def afficher_resultat(vainqueur):
         
     elif vainqueur == 2:
         print("Félicitations ! Le joueur 2 a gagné la manche.") 
-###
+
